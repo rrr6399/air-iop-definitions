a14cda9e1e660293edfefb48c079c61b749751fd
robert.ratcliff <rob@futuretek.com>
2024-05-20 13:07:26 -0500

    Add command to turn off/on battery backup on GCU

--- generate.bat
+++ generate.bat
@@ -5,6 +5,6 @@ rem python2.7 -m mavgenerate
 rem python -m pymavlink.tools.mavgen --lang=C++11 --wire-protocol=2.0 --output=generated/c++/mavlink/v2.0 message_definitions/v1.0/ardupilotmega.xml
 rem python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output=generated/cs/mavlink/v2.0 message_definitions/v1.0/ardupilotmega.xml
 rem
-python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0   --output=generated/python/mavlink/v2.0 message_definitions/v1.0/all.xml
-rem python -m pymavlink.tools.mavgen --lang=Java --wire-protocol=2.0 --output=generated/java/mavlink/v2.0 message_definitions/v1.0/all.xml
+rem python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0   --output=generated/python/mavlink/v2.0 message_definitions/v1.0/all.xml
+python -m pymavlink.tools.mavgen --lang=Java --wire-protocol=2.0 --output=generated/java/mavlink/v2.0 message_definitions/v1.0/all.xml
 pause
